163be4b52ce1c89b9852549aa1dbbe3452ad1704
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2020-09-09 22:41:27 +0300

    Updates

---
+++ precache-manifest.c11b77976e656cadaf2f157fb94217c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dbab1d7310b8076b44162b4e0ec97e5",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "92de2166a404247145a0",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.b6beb33a.chunk.css"
+  },
+  {
+    "revision": "63ab344793d3b083b55a",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.6412118c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.6412118c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92de2166a404247145a0",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.3d48f307.chunk.js"
+  },
+  {
+    "revision": "03184fed29a48f99ce99",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.634f871b.js"
+  }
+]);
