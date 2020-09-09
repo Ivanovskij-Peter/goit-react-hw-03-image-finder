1d4613235d07c07ab61700523340912f3e2165a3
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2020-09-09 22:41:57 +0300

    внес правки

--- src/Images.js
+++ src/Images.js
@@ -4,6 +4,7 @@ import { Search } from './components/searchbar/Searchbar';
 import { ImageGallery } from './components/imageGallery/ImageGallery';
 import { Button } from './components/button/Button';
 import Spinner from './components/loader/Spinner';
+import style from './components/App.module.css';
 
 const API_KEY = '17938696-a211d439281abf74b3f685096';
 
@@ -16,40 +17,39 @@ class Images extends React.Component {
     loading: false,
   };
 
-  getImages = async (qwery, page) => {
+  getImages = async () => {
     try {
       this.setState({ loading: true });
       await axios
         .get(
-          `/?q=${qwery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+          `/?q=${this.state.qwery}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
         )
-        .then(response => this.setState({ images: response.data.hits }));
+        .then(response => {
+          this.setState(prevState => ({
+            images: [...prevState.images, ...response.data.hits],
+            loading: false,
+            page: prevState.page + 1,
+          }));
+        });
     } catch (error) {
       console.log(error);
     } finally {
       this.setState({ loading: false });
+      window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: 'smooth',
+      });
     }
   };
-  componentDidMount() {
-    this.getImages();
-  }
+
   componentDidUpdate(prevProps, prevState) {
     if (prevState.qwery !== this.state.qwery) {
       this.getImages(this.state.qwery);
     }
   }
 
-  scroll() {
-    window.scrollTo({
-      top: document.documentElement.scrollHeight,
-      behavior: 'smooth',
-    });
-  }
-  newPage = () => {
-    this.setState(prev => ({ page: prev.page + 1, loading: true }));
-  };
   getQwery = qwery => {
-    this.setState({ qwery });
+    this.setState({ qwery, images: [], page: 1 });
   };
 
   render() {
@@ -57,9 +57,11 @@ class Images extends React.Component {
     return (
       <>
         <Search getQwery={this.getQwery} />
-        {loading && <Spinner />}
         <ImageGallery data={images} />
-        {images.length > 0 && <Button newPage={this.newPage} />}
+        <div className={style.LoaderDiv}>{loading && <Spinner />}</div>
+        <div className={style.ButtonFather}>
+          {images.length > 0 && !loading && <Button newPage={this.getImages} />}
+        </div>
       </>
     );
   }
